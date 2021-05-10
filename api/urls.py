1e01db341b44383bf41dbbda94a65ee45d1bbe3a
Rafael Jacomel <rafaelmj@ibm.com>
2021-05-10 19:36:03 -0300

    add urls

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
 from django.conf.urls import include
 
+router = routers.DefaultRouter()
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('', include(router.urls)),
 ]
